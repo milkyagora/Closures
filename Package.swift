7931bad9a0c182d301240ce91f3b9ac4d7510bcb
milkyagora <milkyjoyagora@gmail.com>
2025-05-12 10:06:49 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,14 +1,19 @@
 // swift-tools-version:5.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
     name: "Closures",
     products: [
-        .library(name: "Closures",  targets: ["Closures"])
+        .library(name: "Closures", targets: ["Closures"])
     ],
     dependencies: [],
     targets: [
-        .target(name: "Closures", path: "Xcode/Closures")
+        .target(
+            name: "Closures",
+            path: "Xcode/Closures",
+            swiftSettings: [
+                .unsafeFlags(["-Onone"], .when(configuration: .debug))
+            ]
+        )
     ]
 )
